1a114a05c99c1e0721ad84c57e47d5ffc9ef3bdc
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-05-10 17:00:11 +0100

    Add Eclipse workspace

---
+++ Basics/JuInfo.java
@@ -0,0 +1,7 @@
+class JuInfo{
+	public static void main(String args[]){
+		System.out.println("Juliana");
+		System.out.println("London");
+		System.out.println("Mobile number: 0790000007");
+	}
+}
---
+++ Eclipse-workspace/JavaBasic/DisplayValue/src/DisplayValue.java
@@ -0,0 +1,27 @@
+
+public class DisplayValue {
+
+	public static void main(String[] args) {
+			
+//print these values: 10, 56.9, false, S, Prisca, true, 347890, 2147483650
+		
+		int value1=10;
+		float value2=56.9f;
+		boolean value3=false;
+		char value4='S';
+		String value5="Prisca";
+		boolean value6=true;
+		int value7=347890;
+		long value8=2147483650l;
+		
+		System.out.println(value1);
+		System.out.println(value2);
+		System.out.println(value3);
+		System.out.println(value4);
+		System.out.println(value5);
+		System.out.println(value6);
+		System.out.println(value7);
+		System.out.println(value8);
+	}
+
+}
