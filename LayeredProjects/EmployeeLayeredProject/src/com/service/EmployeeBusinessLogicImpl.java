7a041522e2e562197dbdc746070cafaf814b2b93
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-05-25 12:47:55 +0100

    Add EmployeeLayeredProject

---
+++ LayeredProjects/EmployeeLayeredProject/src/com/service/EmployeeBusinessLogicImpl.java
@@ -0,0 +1,55 @@
+package com.service;
+
+import java.util.LinkedList;
+
+import com.dto.Employee;
+import com.persistence.EmployeeDataAccess;
+import com.persistence.EmployeeDataAccessImpl;
+
+public class EmployeeBusinessLogicImpl implements EmployeeBusinessLogic {
+
+	
+	private EmployeeDataAccess dataAccess=new EmployeeDataAccessImpl();
+	
+	private LinkedList<Employee> employeeList;
+	
+	public EmployeeBusinessLogicImpl() {
+		try {
+			employeeList=dataAccess.readRecords();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public LinkedList<Employee> getAllEmployees() {
+		
+		return employeeList;
+	}
+
+	@Override
+	public boolean addEmployee(Employee employee) {
+		
+		return employeeList.add(employee);
+	}
+
+	@Override
+	public boolean deleteEmployee(int id) {
+		for(Employee employee:employeeList) {
+			if(id==employee.getEmpId())
+				return employeeList.remove(employee);
+		}
+		return false;
+	}
+	
+	public void saveAllEmployees() {
+		try {
+			dataAccess.writeRecords(employeeList);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+}
