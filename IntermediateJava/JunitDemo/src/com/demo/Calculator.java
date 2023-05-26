2e440d7266d887baa9789a556d2a5c794533a977
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-05-26 15:01:42 +0100

    Add JUnitDemo

---
+++ IntermediateJava/JunitDemo/src/com/demo/Calculator.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import com.exception.NegativeNumberException;
+
+public class Calculator {
+
+	/*
+	 * Requirement (R001) divide will divide two positive integers if number1 is
+	 * negative : NegativeNumberException if number2 is negative :
+	 * NegativeNumberException
+	 * 
+	 */
+	public int divide(int number1, int number2) throws NegativeNumberException {
+
+		if (number1 >= 0 && number2 >= 0)
+			return number1 / number2;
+		else
+			throw new NegativeNumberException("Number cannot be Negative");
+
+	}
+}
---
+++ IntermediateJava/JunitDemo/src/com/test/CalculatorTest.java
@@ -0,0 +1,60 @@
+package com.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.demo.Calculator;
+import com.exception.NegativeNumberException;
+
+class CalculatorTest {
+
+	//refrence variable : null
+	private Calculator calculator;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		//we created the object and now my reference variable is referring/pointing to that object on heap
+		calculator=new Calculator();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		//reference variable :null
+		calculator=null;
+	}
+
+	@Test
+	void r001T001() throws NegativeNumberException {
+		//assertEquals(expected,function call)
+		assertEquals(6, calculator.divide(12, 2));
+		
+	}
+	
+	@Test
+	void r001T002()throws NegativeNumberException{
+		assertThrows(NegativeNumberException.class, ()->calculator.divide(-12, 2));
+	}
+	
+	@Test
+	void r001T003()throws NegativeNumberException{
+		assertThrows(NegativeNumberException.class, ()->calculator.divide(12, -2));
+	}
+
+	@Test
+	void r001T004()throws NegativeNumberException{
+		assertThrows(NegativeNumberException.class, ()->calculator.divide(-12, -2));
+	}
+	@Test
+	void r001T005() throws NegativeNumberException {
+		assertEquals(0, calculator.divide(0, 5));
+	}
+	
+
+	@Test
+	void r001T006()throws NegativeNumberException{
+		assertThrows(ArithmeticException.class, ()->calculator.divide(5, 0));
+	}
+}
