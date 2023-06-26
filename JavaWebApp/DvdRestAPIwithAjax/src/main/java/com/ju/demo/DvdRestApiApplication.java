b210bfe8f75974861074ce2bda84cacac45ffcdc
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-06-26 16:32:46 +0100

    Remove Employee project

--- JavaWebApp/DvdRestAPIwithAjax/src/main/java/com/ju/demo/DvdRestApiApplication.java
+++ JavaWebApp/DvdRestAPIwithAjax/src/main/java/com/ju/demo/DvdRestApiApplication.java
@@ -1,14 +1,13 @@
 package com.ju.demo;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.ju")
-public class EmployeeRestApiApplication {
+public class DvdRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeRestApiApplication.class, args);
+		SpringApplication.run(DvdRestApiApplication.class, args);
 	}
 
 }
--- JavaWebApp/DvdRestAPIwithAjax/src/test/java/com/ju/demo/DvdRestApiApplicationTests.java
+++ JavaWebApp/DvdRestAPIwithAjax/src/test/java/com/ju/demo/DvdRestApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeeRestApiApplicationTests {
+class DvdRestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
