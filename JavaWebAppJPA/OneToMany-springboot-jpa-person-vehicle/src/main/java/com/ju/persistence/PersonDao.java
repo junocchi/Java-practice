98924432ae0979b93f0e4bcaeaedfa0110979b7f
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-07-05 17:31:26 +0100

    Implement PersonDao and VehicleDao

--- JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/persistence/PersonDao.java
+++ JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/persistence/PersonDao.java
@@ -1,5 +1,11 @@
 package com.ju.persistence;
 
-public interface PersonDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ju.dto.entity.Person;
+
+@Repository
+public interface PersonDao extends JpaRepository<Person, Integer> {
 
 }
--- JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/persistence/VehicleDao.java
+++ JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/persistence/VehicleDao.java
@@ -1,5 +1,11 @@
 package com.ju.persistence;
 
-public interface VehicleDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ju.dto.entity.Vehicle;
+
+@Repository
+public interface VehicleDao extends JpaRepository<Vehicle, Integer> {
 
 }
