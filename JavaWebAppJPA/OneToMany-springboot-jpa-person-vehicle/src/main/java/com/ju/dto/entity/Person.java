396c0c7a9d9a580d9f246bf111e298f62b0101a4
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-07-05 17:22:28 +0100

    Implement dto.entitty --> Vehicle

--- JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/dto/entity/Person.java
+++ JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/dto/entity/Person.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "person")
+@Table(name = "NewPerson")
 public class Person {
 	
 	@Id
--- JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/dto/entity/Vehicle.java
+++ JavaWebAppJPA/OneToMany-springboot-jpa-person-vehicle/src/main/java/com/ju/dto/entity/Vehicle.java
@@ -1,5 +1,83 @@
 package com.ju.dto.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "NewVehicle")
 public class Vehicle {
+
+	@Id
+	@Column
+	private int vehicleNo;
+	@Column
+	private String type;
+	@Column
+	private String model;
+	@Column
+	private String color;
+	@OneToMany
+	private List<Person> personList = new ArrayList<Person>();
+
+	public Vehicle(int vehicleNo, String type, String model, String color, List<Person> personList) {
+		super();
+		this.vehicleNo = vehicleNo;
+		this.type = type;
+		this.model = model;
+		this.color = color;
+		this.personList = personList;
+	}
+
+	public int getVehicleNo() {
+		return vehicleNo;
+	}
+
+	public void setVehicleNo(int vehicleNo) {
+		this.vehicleNo = vehicleNo;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public List<Person> getPersonList() {
+		return personList;
+	}
+
+	public void setPersonList(List<Person> personList) {
+		this.personList = personList;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [vehicleNo=" + vehicleNo + ", type=" + type + ", model=" + model + ", color=" + color
+				+ ", personList=" + personList + "]";
+	}
 
 }
